0b3eefac6c43595ddd3c62bf9be4c9297a512e13
ChrisV <christianj.villa@yahoo.com>
2019-05-13 21:17:33 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,20 +3,21 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-
-cardsInPlay.push(cardOne);
-
-console.log("user flipped queen");
-
-var cardTwo = cards[2];
-
-cardsInPlay.push(cardTwo);
-
-console.log("user flipped king");
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {	
-console.log("You found a match!");	
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
 } else {
-	console.log("Sorry, try again.");
+  console.log("Sorry, try again.");
 }
+}
+function flipCard(cardId) {
+console.log("user flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+if(cardsInPlay.length > 1) {
+	checkForMatch();
+	}	
+}
+
+flipCard(0);
+flipCard(2);
+
